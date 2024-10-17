f682963b932816cdfc8ed71e0d52d6e8a8ec656d
Levente Kovacs <nagyklevi@gmail.com>
2024-10-17 11:11:57 +0200

    FontAwesome, Bootstrap install + Basic Navbar, Sidenav implemented

--- frontend/src/app/common/nav/navbar/navbar.component.ts
+++ frontend/src/app/common/nav/navbar/navbar.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faCoins, faRightFromBracket, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule,
+    FontAwesomeModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  @Input() inputSidenav!: MatSidenav;
+  @Input() inputSidenavState!: boolean;
+
+  faRightToBracket = faRightToBracket;
+  faRightFromBracket = faRightFromBracket;
+  faBars = faBars;
+  faCoins = faCoins;
+
+  prevScrollpos = window.scrollY || window.pageYOffset;
+  isNavOpen: boolean = true;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const currentScrollPos = window.scrollY || window.pageYOffset;
+    if (this.prevScrollpos > currentScrollPos) {
+      this.isNavOpen = true;
+    } else {
+      this.isNavOpen = false;
+    }
+    this.prevScrollpos = currentScrollPos;
+  }
+
 
 }
--- frontend/src/app/common/nav/sidenav/sidenav.component.ts
+++ frontend/src/app/common/nav/sidenav/sidenav.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { faDiscord, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    FontAwesomeModule,
+    RouterModule
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
+
+  @Input() inputSidenavClose!: MatSidenav;
+
+  faFacebook = faFacebookF;
+  faInstagram = faInstagram;
+  faDiscord = faDiscord;
+  faXmark = faXmark;
 
 }
