bb2eb28beddb4d377b278f80fe2ba7a55b44fd2b
Levente Kovacs <nagyklevi@gmail.com>
2024-10-21 15:11:30 +0200

    Quiz Model & Service implemented

---
+++ frontend/src/app/model/quiz.ts
@@ -0,0 +1,10 @@
+export class Quiz {
+  id: number = 0;
+  question: string = '';
+  answer1: string = '';
+  answer2: string = '';
+  answer3: string = '';
+  answer4: string = '';
+  correctAnswerId: number = 0;
+  active: boolean = true
+}
