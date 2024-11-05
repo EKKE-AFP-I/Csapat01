30264efdc1beb3d754298f3643b0b0735ae0ab3c
Levente Kovacs <nagyklevi@gmail.com>
2024-11-05 16:29:42 +0100

    Admin Pages created + Nav Routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { QuestionComponent } from './page/question/question.component';
+import { AdminHomeComponent } from './page/admin-home/admin-home.component';
+import { AdminEditComponent } from './page/admin-edit/admin-edit.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +14,16 @@ export const routes: Routes = [
     path: 'question',
     title: 'Question',
     component: QuestionComponent,
+  },
+  {
+    path: 'admin-home',
+    title: 'Admin Home',
+    component: AdminHomeComponent,
+  },
+  {
+    path: 'admin-edit',
+    title: 'Admin Editor',
+    component: AdminEditComponent,
   },
   {
     path: '**',
