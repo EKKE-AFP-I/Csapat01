a75f64002695f7c60568d097e17841b5f1604c62
fViktor86 <f.viktor1986@gmail.com>
2025-01-06 16:26:25 +0100

    Defined databes basics

--- backend/connection.php
+++ backend/connection.php
@@ -1,2 +1,8 @@
 <?php 
 
+define('DB_HOST', 'localhost');
+define('DB_PORT', 4444);
+define('DB_NAME', 'afp_project');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_DRIVER', 'mysql');
