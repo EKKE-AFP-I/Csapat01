4eef293275e65f9f324dab6aab2f55c9a3977994
fViktor86 <f.viktor1986@gmail.com>
2025-01-08 16:29:13 +0100

    Modified DB_NAME in connection.php

--- backend/connection.php
+++ backend/connection.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 4444);
-define('DB_NAME', 'afp_project');
+define('DB_NAME', 'csapat01');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_DRIVER', 'mysql');
