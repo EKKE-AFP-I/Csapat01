4fe05da85c5728f1e9af19267aba1b261117ee92
fViktor86 <f.viktor1986@gmail.com>
2025-01-08 16:37:01 +0100

    SELECT all user with functions

--- backend/userLekerdez.php
+++ backend/userLekerdez.php
@@ -1,6 +1,12 @@
 <?php
     require_once 'connection.php';
-    $querry = 'SELECT * FROM users;';
-    $users = execute_ql_list($querry);
-    echo json_encode($users);
+
+    function get_all_users() { 
+        $query = 'SELECT * FROM users;'; 
+        $users = execute_ql_list($query); 
+        return json_encode($users); 
+    }
+
+    $all_users = get_all_users(); 
+    echo $all_users;
 ?>
