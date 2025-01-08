34da0c44b6a0c74b15e380c0e284ab3915793d5f
fViktor86 <f.viktor1986@gmail.com>
2025-01-08 16:38:00 +0100

    Created get_user_by_id function

--- backend/userLekerdez.php
+++ backend/userLekerdez.php
@@ -6,7 +6,16 @@
         $users = execute_ql_list($query); 
         return json_encode($users); 
     }
+    function get_user_by_id($id) { 
+        $query = 'SELECT * FROM users WHERE id = :id;'; 
+        $query_params = ['id' => $id]; 
+        $user = execute_ql_list($query, $query_params); 
+        return json_encode($user); 
+    }
 
     $all_users = get_all_users(); 
     echo $all_users;
+
+    $user = get_user_by_id(1); 
+    echo $user;
 ?>
