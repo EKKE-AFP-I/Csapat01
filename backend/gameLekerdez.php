54549d4d649509b00d53c45279de85ee934b298b
fViktor86 <f.viktor1986@gmail.com>
2025-01-10 17:28:23 +0100

    Finished get_all_games function

--- backend/gameLekerdez.php
+++ backend/gameLekerdez.php
@@ -2,5 +2,11 @@
     require_once 'connection.php';
 
     function get_all_games() { 
-        
+        $query = 'SELECT * FROM games;'; 
+        $games = execute_ql_list($query); 
+        return json_encode($games); 
     }
+
+    $all_games = get_all_games();
+    echo "All games: <br>";
+    echo $all_games;
