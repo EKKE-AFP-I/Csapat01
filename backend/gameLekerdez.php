e1e875cb46be5acb0e97ed0c9798d6c7ce665431
fViktor86 <f.viktor1986@gmail.com>
2025-01-10 17:31:20 +0100

    Finished get_games_by_score function

--- backend/gameLekerdez.php
+++ backend/gameLekerdez.php
@@ -13,7 +13,10 @@
         return json_encode($game); 
     }
     function get_games_by_score($game_cnt) { 
-        
+        $query = 'SELECT * FROM games ORDER BY score DESC LIMIT :game_cnt;'; 
+        $query_params = ['game_cnt' => $game_cnt]; 
+        $games = execute_ql_list($query, $query_params); 
+        return json_encode($games);
     }
 
     $all_games = get_all_games();
@@ -23,3 +26,7 @@
     $game = get_game_by_id(1);
     echo "Game with id 1: <br>";
     echo $game;
+
+    $best_games = get_games_by_score(3);
+    echo "Best 3 games: <br>";
+    echo $best_games;
