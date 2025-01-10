127836d47539a4e41db5b95ee217e6b512a3b08d
fViktor86 <f.viktor1986@gmail.com>
2025-01-10 17:29:50 +0100

    Finished get_game_by_id function

--- backend/gameLekerdez.php
+++ backend/gameLekerdez.php
@@ -7,9 +7,16 @@
         return json_encode($games); 
     }
     function get_game_by_id($id) { 
-        
+        $query = 'SELECT * FROM games WHERE id = :id;'; 
+        $query_params = ['id' => $id]; 
+        $game = execute_ql_list($query, $query_params); 
+        return json_encode($game); 
     }
 
     $all_games = get_all_games();
     echo "All games: <br>";
     echo $all_games;
+
+    $game = get_game_by_id(1);
+    echo "Game with id 1: <br>";
+    echo $game;
