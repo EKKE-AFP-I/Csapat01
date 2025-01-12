fd9681e89743b91e1fa197667992378152cc822c
attila0329 <hajduattila0329@gmail.com>
2025-01-12 15:03:32 +0100

    Created get_all_questions function

--- backend/getQuestion.php
+++ backend/getQuestion.php
@@ -1,3 +1,9 @@
 <?php
+    require_once 'connection.php';
 
+    function get_all_questions() { 
+        $query = 'SELECT * FROM quiz;'; 
+        $questions = execute_ql_list($query); 
+        return json_encode($questions); 
+    }
 ?>
