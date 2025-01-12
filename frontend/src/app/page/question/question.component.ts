ce432604d7a675d3e038ea89e7478655e1ea7d39
Levente Kovacs <nagyklevi@gmail.com>
2025-01-12 13:44:01 +0100

    Quiz mechanism perfected + some UX/UI around the app

--- frontend/src/app/page/question/question.component.ts
+++ frontend/src/app/page/question/question.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { QuizService } from '../../service/quiz.service';
 import { CommonModule } from '@angular/common';
-import { AnswerComponent } from '../../common/answer/answer.component';
-// import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { faL } from '@fortawesome/free-solid-svg-icons';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -24,16 +22,15 @@ export class QuestionComponent {
   entity = 'quiz';
   list$ = this.quizService.getAll();
 
-  questionCount: number = 10;
+  questionCount: number = 3;
+  correctAnswersCount: number = 0;
   currentQuizId: number = 0;
-  answerSelected: boolean = false;
-  selectedAnswer: string = '';
-  success: number = 0;
+  isAnswerSelected: boolean = false;
+  // selectedAnswer: string = '';
   classSuccess1: string = '';
   classSuccess2: string = '';
   classSuccess3: string = '';
   classSuccess4: string = '';
-
 
   constructor(
     private quizService: QuizService,
@@ -49,46 +46,50 @@ export class QuestionComponent {
 
   }
 
-
-
   async selectAnswer(id: number, isCorrect: boolean) {
-    this.answerSelected = true;
-    isCorrect ? this.success = 1 : this.success = -1;
+    this.isAnswerSelected = true;
+    if (isCorrect) {
+      this.correctAnswersCount++;
+    }
     switch (id) {
       case 1:
         isCorrect ? this.classSuccess1 = 'correct-answer' : this.classSuccess1 = 'incorrect-answer';
         break;
       case 2:
-          isCorrect ? this.classSuccess1 = 'correct-answer' : this.classSuccess2 = 'incorrect-answer';
+          isCorrect ? this.classSuccess2 = 'correct-answer' : this.classSuccess2 = 'incorrect-answer';
         break;
       case 3:
-        isCorrect ? this.classSuccess1 = 'correct-answer' : this.classSuccess3 = 'incorrect-answer';
+        isCorrect ? this.classSuccess3 = 'correct-answer' : this.classSuccess3 = 'incorrect-answer';
         break;
       case 4:
-        isCorrect ? this.classSuccess1 = 'correct-answer' : this.classSuccess4 = 'incorrect-answer';
+        isCorrect ? this.classSuccess4 = 'correct-answer' : this.classSuccess4 = 'incorrect-answer';
         break;
     }
 
     await this.waitForMiliseconds(600);
-    this.answerSelected = false;
-    this.success = 0;
+    this.isAnswerSelected = false;
+    // this.success = 0;
     this.classSuccess1 = '';
     this.classSuccess2 = '';
     this.classSuccess3 = '';
     this.classSuccess4 = '';
 
-    if (this.currentQuizId < this.questionCount) {
+    if (this.currentQuizId+1 < this.questionCount) {
       this.currentQuizId++;
     } else {
-      this.router.navigate([''])
+      this.router.navigate(['/result'], { queryParams: {
+          questionCount: this.questionCount,
+          correctAnswersCount: this.correctAnswersCount
+        }})
     }
-    // if (isCorrect) {
-    // }
-
-
   }
 
+  // [M][I][K][L][Ó][S]
 
+
+  private waitForMiliseconds(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
   // setClass1(success: number): string {
   //   if (success == 1) {
@@ -126,7 +127,4 @@ export class QuestionComponent {
   //   return '';
   // }
 
-  private waitForMiliseconds(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
 }
--- frontend/src/app/page/result/result.component.ts
+++ frontend/src/app/page/result/result.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-result',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './result.component.html',
   styleUrl: './result.component.scss'
 })
 export class ResultComponent {
+
+  questionCount: number = 0;
+  correctAnswersCount: number = 0;
+
+  constructor (
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.questionCount = +params['questionCount']; // Retrieve and store the passed value
+      this.correctAnswersCount = +params['correctAnswersCount']; // Retrieve and store the passed value
+    });
+  }
 
 }
