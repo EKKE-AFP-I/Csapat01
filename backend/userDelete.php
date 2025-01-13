0dc1a3b8e5bceca637e6f45cb25c4739b4412721
fViktor86 <f.viktor1986@gmail.com>
2025-01-13 20:44:43 +0100

    Created delete_game_by_id function

--- backend/userDelete.php
+++ backend/userDelete.php
@@ -5,3 +5,9 @@
         $query = 'DELETE FROM games;'; 
         execute_dml($query); 
     }
+
+    function delete_game_by_id($id) { 
+        $query = 'DELETE FROM games WHERE id = :id;'; 
+        $query_params = ['id' => $id]; 
+        execute_dml($query, $query_params); 
+    }
