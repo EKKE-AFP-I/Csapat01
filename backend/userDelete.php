1215ec93a281312b6a0196ce8717304760eb15cb
fViktor86 <f.viktor1986@gmail.com>
2025-01-14 21:06:47 +0100

    Created delete_user_by_id function

--- backend/userDelete.php
+++ backend/userDelete.php
@@ -5,3 +5,9 @@
         $query = 'DELETE FROM users;'; 
         execute_dml($query); 
     }
+
+    function delete_user_by_id($id) { 
+        $query = 'DELETE FROM users WHERE id = :id;'; 
+        $query_params = ['id' => $id]; 
+        execute_dml($query, $query_params); 
+    }
