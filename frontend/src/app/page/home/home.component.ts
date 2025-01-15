b32d3db3f3a77109a116204b1fd286b97750e06e
Levente Kovacs <nagyklevi@gmail.com>
2025-01-16 00:34:44 +0100

    Some minor styling on play buttons and h3 header added to Home

--- frontend/src/app/page/home/home.component.ts
+++ frontend/src/app/page/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { QuestionComponent } from "../question/question.component";
+// import { QuestionComponent } from "../question/question.component";
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    QuestionComponent,
+    // QuestionComponent,
     RouterModule
   ],
   templateUrl: './home.component.html',
